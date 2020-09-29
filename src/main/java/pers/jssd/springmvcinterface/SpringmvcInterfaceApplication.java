8c4dceeb1bf29220523eed2020c29838a2ae26d5
王京京 <wangjingjing@bonc.com.cn>
2020-09-29 19:57:23 +0800

    feat(global): 初始化项目

---
+++ src/main/java/pers/jssd/springmvcinterface/SpringmvcInterfaceApplication.java
@@ -0,0 +1,16 @@
+package pers.jssd.springmvcinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jssd
+ */
+@SpringBootApplication
+public class SpringmvcInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/jssd/springmvcinterface/SpringmvcInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jssd.springmvcinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
